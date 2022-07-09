6f7093f0f7f19ea3f480a17a5b5f8472a8f95e9a
jihane01 <jihanemajd14@gmail.com>
2022-07-09 12:39:22 +0100

    add io

---
+++ io.py
@@ -0,0 +1,10 @@
+""" in this program i will create a madlibs where i will practise inputs and outputs in python"""
+
+
+
+name = input("Enter your name :")
+age =input ("Enter your age :")
+sport = input ("Enter your favorite sport :")
+
+
+print(f"Hello my names is {name} ,i'm {age} years old and in my free time i like to play {sport}")
