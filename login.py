3bf004bd19aee23aec1b1c0bcd4149e1bbb07262
jihane01 <jihanemajd14@gmail.com>
2022-07-09 13:39:24 +0100

    adding login file

---
+++ login.py
@@ -0,0 +1,9 @@
+email = input("Enter your email :")
+password = input("Enter your password :")
+def check_login():
+    if email == "j@gmail.com" and password == "hi":
+        print("correct")
+    else:
+        print("REtry")
+
+check_login()
